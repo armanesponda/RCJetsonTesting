f2553536a48288f2855e0fd1a8bf1ab08c1e988d
armanesponda <armanesponda@gmail.com>
2026-04-20 11:55:25 -0500

    Update GPIO pin assignments for left motor

--- gpio_test.py
+++ gpio_test.py
@@ -3,9 +3,9 @@ os.environ['JETSON_MODEL'] = 'JETSON_ORIN_NANO'
 import Jetson.GPIO as GPIO
 import time
 
-ENA = 6  # Left motor PWM (Pin 33)
-IN1 = 5  # Left direction 1 (Pin 35)
-IN2 = 24  # Left direction 2 (Pin 37)
+ENA = 6   # Pin 31
+IN1 = 5   # Pin 29
+IN2 = 24  # Pin 18
 ENB = 12  # Right motor PWM (Pin 32)
 IN3 = 16  # Right direction 1 (Pin 36)
 IN4 = 20  # Right direction 2 (Pin 38)
